2261e218dbbd3297e536e215dd73b23ec7feb3ac
jstutz4 <stu15012@gmail.com>
2021-01-16 14:36:09 -0800

    main nav component

---
+++ src/components/nav-header.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react'
+
+const ulCSS = {
+    listStyleType: "none",
+    margin: "0",
+    paddingLeft: "0",
+}
+
+const navHorizontal = {
+    float: "left",
+    padding: "0 7px",
+}
+
+const navColor = {
+    border: "3px green solid",
+    borderRadius: "6px",
+    float: "left",
+    backgroundColor: "rgba(0,200,0, .3)",
+    padding: "3px",
+    marginTop: "5vh",
+}
+
+const mainNavNames = 
+[
+    {name:"Getting Started", link:"#"}, 
+    {name:"Saving", link:"#"},
+    {name:"Investing", link:"#"},
+    {name:"About", link:"#"},
+]
+
+const linkItems = [];
+
+mainNavNames.forEach((item) => {
+    linkItems.push(<li key={item.name} style={navHorizontal}><a href={item.link}>{item.name}</a></li>);
+})
+
+export default function NavHeader(props) {
+    return(
+        <nav style={navColor}>
+            <ul style={ulCSS}>
+                {linkItems}
+            </ul>
+        </nav>
+    );
+}
