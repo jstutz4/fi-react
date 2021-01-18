22791d063162fac88ea1f2eee50fe6375d1cb1aa
jstutz4 <stu15012@gmail.com>
2021-01-18 08:56:46 -0800

    added article section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import NavHeader from './components/nav-header'
+import aboutArticle from './components/article'
 
 function App() {
   return (
     <div className="center">
       {NavHeader()}
+      {aboutArticle()}
     </div>
   );
 }
