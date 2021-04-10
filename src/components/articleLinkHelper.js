d6bce3da4545a57a3be6fc5e6a0808efd738c691
jstutz4 <stu15012@gmail.com>
2021-04-10 16:24:43 -0700

    capitalize link helper component

--- src/components/articleLinkHelper.js
+++ src/components/articleLinkHelper.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {useState, useEffect} from 'react'
 
-export default function articleLinkHelper(props, activeArticle, setActiveArticle, setLastArticle){
+export default function ArticleLinkHelper(props, activeArticle, setActiveArticle, setLastArticle){
     // const [navLinks, setNavLinks] = useState([<li></li>])
     let articleLinks = []
     let navLength = 0
