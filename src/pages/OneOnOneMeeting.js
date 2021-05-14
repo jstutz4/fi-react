f447d651e3dec98ce127ca7023f28b3ab1cb28cf
jstutz4 <stu15012@gmail.com>
2021-05-14 11:23:32 -0700

    remove onclick iframe for meeting schedule

--- src/pages/OneOnOneMeeting.js
+++ src/pages/OneOnOneMeeting.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default function OneOnOneMeeting(props){
-    function openSchedule (e) {
-        document.getElementsByTagName("a")[0].click
-        console.log("there was a click")
-    }
+    // function openSchedule (e) {
+    //     document.getElementsByTagName("a")[0].click
+    //     console.log("there was a click")
+    // }
 
     return(
         <React.Fragment>
@@ -13,7 +13,7 @@ export default function OneOnOneMeeting(props){
             </header>
             {/* use a hard value for the height since height 100% wont work with iframe  */}
             <a href="https://m.supersaas.com/schedule/EmpowerFIu/One_on_One_Meeting" target="_blank" rel="noopener noreferrer" className="background_color schedule" style={{height: '1000px'}} >
-                <iframe width="100%" height="100%" src="https://m.supersaas.com/schedule/EmpowerFIu/One_on_One_Meeting" onClick={openSchedule}>
+                <iframe width="100%" height="100%" src="https://m.supersaas.com/schedule/EmpowerFIu/One_on_One_Meeting">
                 </iframe>
             </a>
             
